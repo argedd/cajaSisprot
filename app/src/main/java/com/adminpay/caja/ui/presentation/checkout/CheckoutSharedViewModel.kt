9e17253c1b7c4a01b28d8b369904ae3b4c0498c3
argedd <46687437+argedd@users.noreply.github.com>
2025-06-11 22:36:14 -0400

    [Add] Bank Associated

--- app/src/main/java/com/adminpay/caja/ui/presentation/checkout/CheckoutSharedViewModel.kt
+++ app/src/main/java/com/adminpay/caja/ui/presentation/checkout/CheckoutSharedViewModel.kt
@@ -2,6 +2,7 @@ package com.adminpay.caja.ui.presentation.checkout
 
 
 import androidx.lifecycle.ViewModel
+import com.adminpay.caja.domain.model.contract.BankAssociated
 import com.adminpay.caja.domain.model.invoice.InvoiceModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
@@ -9,4 +10,5 @@ import javax.inject.Inject
 @HiltViewModel
 class CheckoutSharedViewModel @Inject constructor() : ViewModel() {
     var selectedInvoice: InvoiceModel? = null
+    var bankAssociated: BankAssociated? = null
 }
